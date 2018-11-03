3c9fddabaa8e1cf896aeb144da07259642850029
falgard <mikael.falgard@gmail.com>
2018-11-03 01:08:30 +0100

    globals is not secret

---
+++ lambda/custom/globals.js
@@ -0,0 +1,23 @@
+exports.EVENTTYPES = {
+    HOCKEY: ['Hockey', 'Ice-hockey', 'Icehockey', 'Ishockey'],
+    FOOTBALL: ['Football', 'soccer', 'fotboll']
+};
+
+exports.TEAM_NAMES = [
+  'dif',
+  'djurgården',
+  'diffen'
+]
+
+exports.ARENAS = [
+  'tele2 arena',
+  'hovet',
+  'johanneshov',
+  'globen',
+  'stockholms stadion'
+];
+
+exports.URLS = {
+  HOCKEY: 'http://www.difhockey.se/calendar/29/show/dif.ics',
+  FOOTBALL: 'https://www.google.com/calendar/ical/6q2n1dbd72cvc21jqjglcjpb48%40group.calendar.google.com/public/basic.ics' 
+};
